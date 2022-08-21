b213500dc604f9d2e8622750ff9c7cfb56ba2e9a
Michał Pniak <pniak.michal@gmail.com>
2022-08-21 17:16:44 +0200

    Fixed styles and add some text.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         './docs/*.html'
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                brand: {
+                    50: '#DCEEFB',
+                    100: '#B6E0FE',
+                    200: '#84C5F4',
+                    300: '#62B0E8',
+                    400: '#4098D7',
+                    500: '#2680C2',
+                    600: '#186FAF',
+                    700: '#0F609B',
+                    800: '#0A558C',
+                    900: '#003E6B',
+                }
+            }
+        },
     },
     plugins: [],
 }
